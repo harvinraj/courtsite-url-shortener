af8195df5adc372f08730da71f13e69c38a19f26
Harvin Raj <harvinraj@Harvins-MacBook-Air.local>
2026-09-06 23:36:35 +0800

    refactor(url-shortener): establish base domain entity and repository interfaces

---
+++ internal/shortener/repository.go
@@ -0,0 +1,23 @@
+package shortener
+
+import (
+	"context"
+	"errors"
+	"net/http"
+)
+
+var (
+	ErrNotFound = errors.New("record not found")
+)
+
+// Repository defines the data access contract for URL records.
+type Repository interface {
+	Save(ctx context.Context, shortener URLShortener) error
+	FindByShortKey(ctx context.Context, shortKey string) (URLShortener, error)
+	RecordVisit(ctx context.Context, key string) (URLShortener, error)
+	GetRecordVisits(ctx context.Context, key string) (int64, error)
+}
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
